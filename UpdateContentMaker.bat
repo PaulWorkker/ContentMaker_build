5e35cd4768ca384a39764f0dbeba8b652df28654
Pavel <paulworker3d@gmail.com>
2025-08-13 12:38:44 +0300

    change update bat file

--- UpdateContentMaker.bat
+++ UpdateContentMaker.bat
@@ -9,15 +9,7 @@ set HOME=%USERPROFILE%
 @echo.
 @echo Обновление проекта ContentMaker ...
 @echo.
-git pull -f
-
-if exist %local_repo_path% (
-    @echo.
-    @echo Обновление MDT плагинов ...
-    @echo.
-    
-    cd %local_repo_path%
-    git pull -f
-)
+git fetch
+git reset origin/master --hard
 
 pause >nul
